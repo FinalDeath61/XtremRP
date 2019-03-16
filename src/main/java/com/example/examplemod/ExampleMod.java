59da7f7886b02c5c66e458ba935c7025bef1fdb0
Pocheaniol <jeremiric@hotmail.fr>
2019-03-16 19:22:38 +0100

    Modification nom

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,9 +10,9 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "xtremrp";
+    public static final String NAME = "Xtrem Rp";
+    public static final String VERSION = "0.1";
 
     private static Logger logger;
 
